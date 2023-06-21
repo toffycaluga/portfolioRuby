ffedcc1feee5f2773983708ceb245882016dee6d
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-21 17:08:28 -0300

    feat: configurando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/projects'
-  get 'pages/contact'
+  get 'pages/home', to:'pages#home'
+  get 'pages/projects', to:'pages#projects  '
+  get 'pages/contact',to:'pages#contact'
+  root 'pages#home'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
