54ef8cbc5e2142ef5e63203518cede3824ef0104
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-21 21:10:56 -0300

    Feat: Nav bar creado , con icono propio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home', to:'pages#home'
-  get 'pages/projects', to:'pages#projects  '
-  get 'pages/contact',to:'pages#contact'
+  get '/home', to:'pages#home'
+  get '/projects', to:'pages#projects'
+  get '/contact',to:'pages#contact'
   root 'pages#home'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
